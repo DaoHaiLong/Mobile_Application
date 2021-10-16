e4cb70c7fcc8038983d58daad7005428466b9ddd
DaoHaiLong <daohailong.06122000@gmail.com>
2021-10-16 14:25:07 +0700

    mid

---
+++ FaceBook/app/src/main/java/usth/facebook/MainActivity.java
@@ -0,0 +1,73 @@
+package usth.facebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String mess="Android";
+    private int[] tabIcons = {
+            R.drawable.ic_tab_newfeed,
+            R.drawable.ic_tab_pro,
+            R.drawable.ic_tab_watch,
+            R.drawable.ic_tab_notification,
+            R.drawable.ic_tab_menu
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        PagerAdapter adapter = new HomePageAdapterFacebook(getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(5);
+        pager.setAdapter(adapter);
+    }
+    /** Called when the activity is about to become visible. */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Print Log
+        Log.i(mess,"onStart");
+    }
+
+    /** Called when the activity is no longer visible. */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Print Log
+        Log.i(mess,"onStop");
+    }
+
+    /** Called just before the activity is destroyed. */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Print Log
+        Log.i(mess,"onDestroy");
+    }
+    /** Called when another activity is taking focus. */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Print Log
+        Log.i(mess,"onPause");
+    }
+    /** Called when the activity has become visible. */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Print Log
+        Log.i(mess,"onResume");      }
+
+
+    /** Called just before the activity is restarted. */
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        // Print Log
+        Log.i(mess,"onRestart");
+    }
+}
